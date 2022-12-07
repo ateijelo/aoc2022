46438c4e5c107cfd8f5fcfadd40807cf90a41d9e
Andy Teijelo <ateijelo@gmail.com>
2022-12-07 01:20:12 -0500

    day 5, part 1

--- src/main.rs
+++ src/main.rs
@@ -1,97 +1,201 @@
-use std::io::{self, BufRead};
+use std::{
+    char,
+    io::{self, BufRead},
+};
 
-struct Range {
-    start: i32,
-    end: i32,
+#[derive(Debug, PartialEq, Eq)]
+struct Move {
+    count: usize,
+    from: usize,
+    to: usize,
 }
 
-impl Range {
-    fn new(rangestr: &str) -> Range {
-        let s: Vec<String> = String::from(rangestr)
-            .split('-')
-            .map(|x| x.to_string())
-            .collect();
-        assert_eq!(s.len(), 2);
-        let a = s[0].parse::<i32>().unwrap();
-        let b = s[1].parse::<i32>().unwrap();
-        Range { start: a, end: b }
-    }
-
-    fn _contains(&self, other: &Range) -> bool {
-        self.start <= other.start && self.end >= other.end
-    }
-
-    fn overlaps(&self, other: &Range) -> bool {
-        self.start <= other.end && self.end >= other.start
-    }
+struct Plan {
+    stacks: Vec<String>,
+    instructions: Vec<Move>,
 }
 
-fn parse_input(lines: &Vec<String>) -> Vec<(Range, Range)> {
-    let mut pairs = vec![];
+fn parse_input(lines: &Vec<String>) -> Plan {
+    let mut reading_instructions = false;
+    let mut rows: Vec<String> = vec![];
+    let mut instructions: Vec<Move> = vec![];
+    let mut stack_count = 0;
     for line in lines {
-        let s: Vec<&str> = line.split(',').collect();
-        pairs.push((Range::new(s[0]), Range::new(s[1])));
-    }
-    pairs
-}
+        if line.starts_with(" 1") {
+            reading_instructions = true;
+        }
+        if reading_instructions {
+            let ws: Vec<&str> = line.split_whitespace().collect();
+            if ws.len() != 6 {
+                continue;
+            }
+            let count: usize = ws[1].to_string().parse().unwrap();
+            let from: usize = ws[3].to_string().parse().unwrap();
+            let to: usize = ws[5].to_string().parse().unwrap();
 
-fn solution(pairs: Vec<(Range, Range)>) -> u32 {
-    let mut count = 0;
-    for pair in pairs {
-        let (a, b) = pair;
-        if a.overlaps(&b) {
-            count += 1
+            instructions.push(Move { count, from, to })
+        } else {
+            let mut row = "".to_string();
+            let mut i = 1;
+            let line: Vec<char> = line.chars().collect();
+            while i < line.len() {
+                row.push(line[i]);
+                i += 4;
+                stack_count = std::cmp::max(stack_count, row.len());
+            }
+            rows.push(row);
         }
     }
-    count
+
+    // transpose rows into stacks
+    let mut stacks: Vec<String> = vec![];
+    stacks.resize_with(stack_count, String::default);
+    for y in 0..rows.len() {
+        let row: Vec<char> = rows[rows.len() - 1 - y].chars().collect();
+        for x in 0..stack_count {
+            if x >= row.len() {
+                continue;
+            }
+            let c = row[x];
+            if c != ' ' {
+                stacks[x].push(c);
+            }
+        }
+    }
+
+    Plan {
+        stacks,
+        instructions,
+    }
+}
+
+fn solution(plan: &mut Plan) -> String {
+    println!("stacks: {:?}", &plan.stacks);
+    for instruction in &plan.instructions {
+        for _ in 0..instruction.count {
+            println!("instruction: {:?}", instruction);
+            let c = plan.stacks[instruction.from - 1].pop().unwrap();
+            let to = &mut plan.stacks[instruction.to - 1];
+            to.push(c);
+        }
+    }
+    let mut result = "".to_string();
+    for stack in &mut plan.stacks {
+        result.push(stack.chars().last().unwrap())
+    }
+    result
 }
 
 fn main() {
     let lines = io::stdin().lock().lines();
     let lines = lines.map(|line| line.unwrap()).collect();
-    println!("{:?}", solution(parse_input(&lines)));
+    println!("{:?}", solution(&mut parse_input(&lines)));
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    // #[test]
+    // fn test_ranges() {
+    //     let a = Range::new("5-15");
+    //     let b = Range::new("10-25");
+    //     let c = Range::new("1-30");
+    //     assert_eq!(a.start, 5);
+    //     assert_eq!(a.end, 15);
+    //     assert!(c._contains(&a));
+    //     assert!(c._contains(&b));
+    //     assert!(!a._contains(&b));
+    // }
+    //
+    // #[test]
+    // fn test_overlaps() {
+    //     assert!( Range::new("5-7").overlaps(&Range::new("7-9")));
+    //     assert!( Range::new("2-8").overlaps(&Range::new("3-7")));
+    //     assert!( Range::new("6-6").overlaps(&Range::new("4-6")));
+    //     assert!( Range::new("2-6").overlaps(&Range::new("4-8")));
+    //     assert!( Range::new("2-2").overlaps(&Range::new("2-2")));
+    //
+    //     assert!(!Range::new("1-1").overlaps(&Range::new("2-2")));
+    //     assert!(!Range::new("1-5").overlaps(&Range::new("6-7")));
+    // }
+
     #[test]
-    fn test_ranges() {
-        let a = Range::new("5-15");
-        let b = Range::new("10-25");
-        let c = Range::new("1-30");
-        assert_eq!(a.start, 5);
-        assert_eq!(a.end, 15);
-        assert!(c._contains(&a));
-        assert!(c._contains(&b));
-        assert!(!a._contains(&b));
+    fn test_parse_stacks() {
+        let lines = [
+            "    [D]",
+            "[N] [C]",
+            "[Z] [M] [P]",
+            " 1   2   3 ",
+            "",
+            "move 1 from 2 to 1",
+            "move 3 from 1 to 3",
+            "move 2 from 2 to 1",
+            "move 1 from 1 to 2",
+        ];
+        let lines: Vec<String> = lines.iter().map(|x| x.to_string()).collect();
+        let plan = parse_input(&lines);
+        assert_eq!(plan.stacks, vec!["ZN", "MCD", "P"]);
     }
 
     #[test]
-    fn test_overlaps() {
-        assert!( Range::new("5-7").overlaps(&Range::new("7-9")));
-        assert!( Range::new("2-8").overlaps(&Range::new("3-7")));
-        assert!( Range::new("6-6").overlaps(&Range::new("4-6")));
-        assert!( Range::new("2-6").overlaps(&Range::new("4-8")));
-        assert!( Range::new("2-2").overlaps(&Range::new("2-2")));
-
-        assert!(!Range::new("1-1").overlaps(&Range::new("2-2")));
-        assert!(!Range::new("1-5").overlaps(&Range::new("6-7")));
+    fn test_parse_instructions() {
+        let lines = [
+            "    [D]",
+            "[N] [C]",
+            "[Z] [M] [P]",
+            " 1   2   3 ",
+            "",
+            "move 1 from 2 to 1",
+            "move 3 from 1 to 3",
+            "move 2 from 2 to 1",
+            "move 1 from 1 to 2",
+        ];
+        let lines: Vec<String> = lines.iter().map(|x| x.to_string()).collect();
+        let plan = parse_input(&lines);
+        assert_eq!(
+            plan.instructions,
+            vec![
+                Move {
+                    count: 1,
+                    from: 2,
+                    to: 1
+                },
+                Move {
+                    count: 3,
+                    from: 1,
+                    to: 3
+                },
+                Move {
+                    count: 2,
+                    from: 2,
+                    to: 1
+                },
+                Move {
+                    count: 1,
+                    from: 1,
+                    to: 2
+                },
+            ]
+        );
     }
 
     #[test]
     fn test_example() {
         let lines = [
-            "2-4,6-8",
-            "2-3,4-5",
-            "5-7,7-9",
-            "2-8,3-7",
-            "6-6,4-6",
-            "2-6,4-8",
+            "    [D]",
+            "[N] [C]",
+            "[Z] [M] [P]",
+            " 1   2   3 ",
+            "",
+            "move 1 from 2 to 1",
+            "move 3 from 1 to 3",
+            "move 2 from 2 to 1",
+            "move 1 from 1 to 2",
         ];
         let lines: Vec<String> = lines.iter().map(|x| x.to_string()).collect();
-        let pairs = parse_input(&lines);
-        assert_eq!(solution(pairs), 4);
+        let mut plan = parse_input(&lines);
+        assert_eq!(solution(&mut plan), "CMZ");
+        println!("stacks: {:?}", &plan.stacks);
     }
 }
