99d7bd195a31fc785d939125b784e706f965abab
Andy Teijelo Pérez <ateijelo@gmail.com>
2022-12-13 13:33:04 -0500

    day 12, part 1

--- src/main.rs
+++ src/main.rs
@@ -1,136 +1,96 @@
-use std::{
-    collections::VecDeque,
-    io::{self, BufRead},
-};
+use std::io::{self, BufRead};
 
-#[derive(Debug)]
-enum MonkeyOp {
-    Add(u64),
-    Mul(u64),
-    Square,
+use pathfinding::prelude::dijkstra;
+
+struct Input {
+    map: Vec<Vec<u32>>,
+    start: Pos,
+    end: Pos,
 }
 
-#[derive(Debug)]
-struct Monkey {
-    items: VecDeque<u64>,
-    op: MonkeyOp,
-    divisibe_by: u64,
-    if_true: usize,
-    if_false: usize,
-    inspections: u64,
-}
-
-impl Monkey {
-    fn new() -> Monkey {
-        Monkey {
-            items: VecDeque::new(),
-            op: MonkeyOp::Square,
-            divisibe_by: 1,
-            if_true: 0,
-            if_false: 0,
-            inspections: 0,
+fn parse_input(lines: &[String]) -> Input {
+    let mut input = Input {
+        map: vec![],
+        start: Pos { x: 0, y: 0 },
+        end: Pos { x: 0, y: 0 },
+    };
+    for (y, line) in lines.iter().enumerate() {
+        let mut row: Vec<u32> = vec![];
+        for (x, c) in line.chars().enumerate() {
+            let v = match c {
+                'S' => {
+                    input.start = Pos {
+                        x: x as i32,
+                        y: y as i32,
+                    };
+                    0
+                }
+                'E' => {
+                    input.end = Pos {
+                        x: x as i32,
+                        y: y as i32,
+                    };
+                    'z' as u32 - 'a' as u32
+                }
+                'a'..='z' => c as u32 - 'a' as u32,
+                _ => 0,
+            };
+            row.push(v);
         }
+        input.map.push(row);
     }
+    input
 }
 
-fn parse_input(lines: &[String]) -> Vec<Monkey> {
-    let mut monkeys: Vec<Monkey> = vec![];
-    for line in lines {
-        let parts: Vec<&str> = line.split_whitespace().collect();
-        match parts[..] {
-            ["Monkey", _] => {
-                monkeys.push(Monkey::new());
-            }
-            ["Starting", "items:", ..] => {
-                monkeys.last_mut().unwrap().items = parts[2..]
-                    .iter()
-                    .map(|x| x.replace(',', "").parse().unwrap())
-                    .collect();
-            }
-            ["Operation:", "new", "=", "old", op, arg] => {
-                let last_monkey = monkeys.last_mut().unwrap();
-                if arg == "old" {
-                    continue;
-                }
-                match op {
-                    "+" => {
-                        last_monkey.op = MonkeyOp::Add(arg.parse().unwrap());
-                    }
-                    "*" => {
-                        last_monkey.op = MonkeyOp::Mul(arg.parse().unwrap());
-                    }
-                    _ => {}
-                }
-            }
-            ["Test:", "divisible", "by", number] => {
-                monkeys.last_mut().unwrap().divisibe_by = number.parse().unwrap();
-            }
-            ["If", "true:", "throw", "to", "monkey", number] => {
-                monkeys.last_mut().unwrap().if_true = number.parse().unwrap();
-            }
-            ["If", "false:", "throw", "to", "monkey", number] => {
-                monkeys.last_mut().unwrap().if_false = number.parse().unwrap();
-            }
-            _ => {}
+#[derive(Clone, Eq, PartialEq, Hash, Debug)]
+struct Pos {
+    x: i32,
+    y: i32,
+}
+
+fn successors(pos: &Pos, map: &[Vec<u32>]) -> Vec<(Pos, usize)> {
+    let height = map.len() as i32;
+    let width = map[0].len() as i32;
+    let mut v: Vec<(Pos, usize)> = vec![];
+    for (dx, dy) in [(-1, 0), (0, -1), (1, 0), (0, 1)] {
+        if pos.x + dx >= width {
+            continue;
         }
-    }
-    monkeys
-}
-
-fn play_round(monkeys: &mut Vec<Monkey>, supermodulo: u64) {
-    for i in 0..monkeys.len() {
-        loop {
-            let mut item;
-            let next_monkey;
-            {
-                let monkey = &mut monkeys[i];
-                if monkey.items.is_empty() {
-                    break;
-                }
-                // monkey grabs item, increase worry
-                item = monkey.items.pop_front().unwrap();
-                monkey.inspections += 1;
-                match monkey.op {
-                    MonkeyOp::Add(value) => item = item % supermodulo + value,
-                    MonkeyOp::Mul(value) => item = item % supermodulo * value,
-                    MonkeyOp::Square => item = item % supermodulo * item,
-                }
-
-                // part 2, worry never decreases
-                item %= supermodulo;
-
-                // perform test
-                if item % monkey.divisibe_by == 0 {
-                    next_monkey = monkey.if_true;
-                } else {
-                    next_monkey = monkey.if_false;
-                }
-            }
-            let m = &mut monkeys[next_monkey];
-            m.items.push_back(item);
+        if pos.y + dy >= height {
+            continue;
         }
+        if pos.x + dx < 0 {
+            continue;
+        }
+        if pos.y + dy < 0 {
+            continue;
+        }
+        let new_pos = Pos {
+            x: pos.x + dx,
+            y: pos.y + dy,
+        };
+        let mut cost = 4000;
+        let current_height = map[pos.y as usize][pos.x as usize];
+        let neighbor_height = map[new_pos.y as usize][new_pos.x as usize];
+        if current_height + 1 >= neighbor_height {
+            cost = 1;
+        }
+        v.push((new_pos, cost));
     }
+    v
 }
 
-fn solution(monkeys: &mut Vec<Monkey>) -> u64 {
-    let supermodulo: u64 = monkeys.iter().map(|m| m.divisibe_by).product();
-    for _ in 0..10000 {
-        play_round(monkeys, supermodulo);
-    }
-    for monkey in monkeys.iter() {
-        println!("{:?}", monkey.inspections);
-    }
-    monkeys.sort_by_key(|monkey| monkey.inspections);
-    monkeys
-        .iter()
-        .rev()
-        .take(2)
-        .map(|monkey| monkey.inspections)
-        .product()
+fn solution(input: Input) -> usize {
+    let result = dijkstra(
+        &input.start,
+        |p| successors(p, &input.map),
+        |p| *p == input.end,
+    );
+    result.expect("no path found").1
 }
 
-fn solve(lines: &[String]) -> u64 {
-    solution(&mut parse_input(lines))
+fn solve(lines: &[String]) -> usize {
+    solution(parse_input(lines))
 }
 
 fn main() {
@@ -157,6 +117,6 @@ mod tests {
             .map(|x| x.unwrap().trim().to_string())
             .filter(|x| !x.is_empty())
             .collect();
-        assert_eq!(solve(&lines), 2713310158);
+        assert_eq!(solve(&lines), 31);
     }
 }
